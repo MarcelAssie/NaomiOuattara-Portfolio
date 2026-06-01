92df546e00607f22c97f4e78a21feb9b6ae19a50
Marcel Assie <168814750+MarcelAssie@users.noreply.github.com>
2026-06-01 14:07:37 +0200

    Add contact page and update navigation links for improved accessibility

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
     path('projects/', views.projects, name='projects'),
     path("projects/<slug:slug>/", views.project_detail, name="project_detail"),
 
